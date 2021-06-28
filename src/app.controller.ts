415b0044a96401d3dbf42be5e507767cf11bddce
gbloch <gaetan.bloch@gmail.com>
2021-06-28 10:16:45 +0200

    feature(report): generate and return newman report

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Post, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Header, Post, UploadedFiles, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Observable } from "rxjs";
+import { Observable, of } from "rxjs";
 import { fromPromise } from "rxjs/internal-compatibility";
 import { NewmanRunSummary } from "newman";
 import { FileFieldsInterceptor } from "@nestjs/platform-express";
+import { ApiProduces } from "@nestjs/swagger";
 
 @Controller()
 export class AppController {
@@ -11,8 +12,9 @@ export class AppController {
   }
 
   @Get()
+  @Header('content-type', 'application/json')
   getResults(): Observable<NewmanRunSummary> {
-    return fromPromise(this.appService.getResults('../res/sig-v2.json'));
+    return fromPromise(this.appService.getResults('./res/sig-v2.json'));
   }
 
   @Post('upload-collections')
@@ -20,10 +22,12 @@ export class AppController {
     { name: 'collections', maxCount: 100 },
     { name: 'environment', maxCount: 1 }
   ]))
+  @ApiProduces('application/html')
+  @Header('content-type', 'application/html')
   uploadCollections(@UploadedFiles() files) {
-    return fromPromise(this.appService.getResultsFromBlob(
+    return this.appService.getResultsFromBlob(
       files.collections[0].buffer.toString(),
       files.environment[0].buffer.toString(),
-    ))
+    )
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -12,8 +12,12 @@ export class AppService {
       run({
           collection: collection,
           environment: environment,
-          reporters: ['htmlextra'],
-          reporter: htmlextra,
+          reporters: ['htmlextra', 'cli'],
+          reporter: {
+            htmlextra: {
+              export: './res/report.html'
+            }
+          },
         }, (err, summary) => {
           if (err) reject('error: ' + err.message ?? 'Unknown error');
           resolve(summary);
@@ -22,16 +26,37 @@ export class AppService {
     });
   }
 
-  getResultsFromBlob(collection: string, environment: string):
-    Promise<NewmanRunSummary> {
+  getReport(collection: string, environment?: string):
+    Promise<{ report: string }> {
+    return new Promise<{ report: string }>((resolve, reject) => {
+      run({
+          collection: collection,
+          environment: environment,
+          reporters: ['htmlextra', 'cli'],
+          reporter: {
+            htmlextra: {
+              export: './res/report.html'
+            }
+          },
+        }, (err, summary) => {
+          if (err) reject('error: ' + err.message ?? 'Unknown error');
+
+          const report = fs.readFileSync('./res/report.html');
+          resolve({ report: report.toString() });
+        }
+      );
+    });
+  }
+
+  getResultsFromBlob(collection: string, environment: string): Promise<{ report: string }> {
     const colFile = 'collection.json';
     const envFile = 'environment.json';
     this.writeFile(colFile, collection);
     this.writeFile(envFile, environment);
-    return this.getResults(colFile, envFile);
+    return this.getReport(colFile, envFile);
   }
 
-  private writeFile(path: string, content: string): void{
+  private writeFile(path: string, content: string): void {
     fs.writeFile(path, content, err => {
       if (err) console.error(err);
     });
