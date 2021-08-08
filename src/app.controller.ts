c6564dd0e101c8b5b01642c29eb3a9616369ac4a
egbl7030 <gaetan.bloch-ext@pole-emploi.fr>
2021-08-08 17:30:18 +0200

    feat(report): report improvements

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,21 @@
-import { Controller, Get, Header, Post, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Header,
+  Post,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AppService } from './app.service';
-import { Observable, of } from "rxjs";
-import { fromPromise } from "rxjs/internal-compatibility";
-import { NewmanRunSummary } from "newman";
-import { FileFieldsInterceptor } from "@nestjs/platform-express";
-import { ApiProduces } from "@nestjs/swagger";
+import { Observable } from 'rxjs';
+import { fromPromise } from 'rxjs/internal-compatibility';
+import { NewmanRunSummary } from 'newman';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
+import { ApiProduces } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {
-  }
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   @Header('content-type', 'application/json')
@@ -18,16 +24,18 @@ export class AppController {
   }
 
   @Post('upload-collections')
-  @UseInterceptors(FileFieldsInterceptor([
-    { name: 'collections', maxCount: 100 },
-    { name: 'environment', maxCount: 1 }
-  ]))
+  @UseInterceptors(
+    FileFieldsInterceptor([
+      { name: 'collections', maxCount: 100 },
+      { name: 'environment', maxCount: 1 },
+    ]),
+  )
   @ApiProduces('application/html')
   @Header('content-type', 'application/html')
   uploadCollections(@UploadedFiles() files) {
     return this.appService.getResultsFromBlob(
       files.collections[0].buffer.toString(),
       files.environment[0].buffer.toString(),
-    )
+    );
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,54 +1,77 @@
 import { Injectable } from '@nestjs/common';
 import { run, NewmanRunSummary } from 'newman';
-import * as fs from "fs";
-
-const htmlextra = require('newman-reporter-htmlextra');
+import * as fs from 'fs';
 
 @Injectable()
 export class AppService {
-  getResults(collection: string, environment?: string):
-    Promise<NewmanRunSummary> {
+  getResults(
+    collection: string,
+    environment?: string,
+  ): Promise<NewmanRunSummary> {
     return new Promise<NewmanRunSummary>((resolve, reject) => {
-      run({
+      run(
+        {
           collection: collection,
           environment: environment,
           reporters: ['htmlextra', 'cli'],
           reporter: {
             htmlextra: {
-              export: './res/report.html'
-            }
+              export: './res/report.html',
+            },
           },
-        }, (err, summary) => {
+        },
+        (err, summary) => {
           if (err) reject('error: ' + err.message ?? 'Unknown error');
           resolve(summary);
-        }
+        },
       );
     });
   }
 
-  getReport(collection: string, environment?: string):
-    Promise<{ report: string }> {
+  getReport(
+    collection: string,
+    environment?: string,
+  ): Promise<{ report: string }> {
     return new Promise<{ report: string }>((resolve, reject) => {
-      run({
+      run(
+        {
           collection: collection,
           environment: environment,
           reporters: ['htmlextra', 'cli'],
           reporter: {
             htmlextra: {
-              export: './res/report.html'
-            }
+              export: './res/report.html',
+            },
           },
-        }, (err, summary) => {
+        },
+        (err, summary) => {
           if (err) reject('error: ' + err.message ?? 'Unknown error');
 
           const report = fs.readFileSync('./res/report.html');
-          resolve({ report: report.toString() });
-        }
+          resolve({
+            report: report
+              .toString()
+              .replace(
+                '<div class="container">\n' +
+                  '            <label>Light</label>\n' +
+                  '            <label id="switch" class="switch">\n' +
+                  '                <input type="checkbox" onchange="toggleTheme()" id="slider">\n' +
+                  '                <span class="slider round"></span>\n' +
+                  '            </label>\n' +
+                  '            <label>Dark</label>\n' +
+                  '        </div>',
+                '',
+              ),
+          });
+        },
       );
     });
   }
 
-  getResultsFromBlob(collection: string, environment: string): Promise<{ report: string }> {
+  getResultsFromBlob(
+    collection: string,
+    environment: string,
+  ): Promise<{ report: string }> {
     const colFile = 'collection.json';
     const envFile = 'environment.json';
     this.writeFile(colFile, collection);
@@ -57,7 +80,7 @@ export class AppService {
   }
 
   private writeFile(path: string, content: string): void {
-    fs.writeFile(path, content, err => {
+    fs.writeFile(path, content, (err) => {
       if (err) console.error(err);
     });
   }
